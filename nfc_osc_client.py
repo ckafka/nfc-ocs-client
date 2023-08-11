83e28855da8907914133be52c0050fe3fb4706bf
Connor Kafka <conkafka@uw.edu>
2023-08-10 17:58:21 -0700

    initial framework

---
+++ nfc_osc_client.py
@@ -0,0 +1,23 @@
+"""
+nfc ocs server
+"""
+
+import nfc.clf
+
+class NfcReader: 
+    """Nfc Neader Class"""
+    
+    def __init__(self) -> None:
+        self.port = None
+        self.
+
+
+class NfcController:
+    """NfcController Class"""
+
+    def __init__(self):
+        self.
+
+    def discover_readers(self):
+        """temp"""
+        pass
