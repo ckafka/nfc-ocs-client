27e049e1c9aa0f27bb2e51cf70a50791a3e9667f
Connor Kafka <conkafka@uw.edu>
2023-08-10 18:39:22 -0700

    poll readers

--- nfc_osc_client.py
+++ nfc_osc_client.py
@@ -8,22 +8,32 @@ import nfc.clf.transport
 
 import errno
 
+class NfcController:
+    def __init__(self) -> None:
+        self.clfs = []
 
-def discover_readers():
-    """Discover devices"""
-    clfs = []
-    for dev in nfc.clf.transport.TTY.find("tty")[0]:
-        path = "tty:{0}".format(dev[8:])
-        try:
-            clf = nfc.ContactlessFrontend(path)
-            clfs.append(clf) 
-            print("found %s" % clf.device)
-        except IOError as error:
-            if error.errno == errno.EACCES:
-                print("access denied")
-            elif error.errno == errno.EBUSY:
-                print("busy")
+    def discover_readers(self):
+        """Discover readers connected via FTDI USB to serial cables"""
+        for dev in nfc.clf.transport.TTY.find("tty")[0]:
+            path = "tty:{0}".format(dev[8:])
+            try:
+                clf = nfc.ContactlessFrontend(path)
+                self.clfs.append(clf) 
+                print("found %s" % clf.device)
+            except IOError as error:
+                if error.errno == errno.EACCES:
+                    print("access denied")
+                elif error.errno == errno.EBUSY:
+                    print("busy")
 
+    def poll_readers(self): 
+        """Poll each reader for a card, print the tag"""
+        for nfc_reader in self.clfs:
+            tag = nfc_reader.connect(rdwr={})
+            print("device %s " % nfc_reader.device)
+            print("found tag %s" % tag)
 
 if __name__ == "__main__":
-    discover_readers()
+    controller = NfcController()
+    while True: 
+        controller.discover_readers()
