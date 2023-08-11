e32c5a77548a3e33d32d5378e03253954c02932e
Connor Kafka <conkafka@uw.edu>
2023-08-10 18:25:03 -0700

    add discovery

--- nfc_osc_client.py
+++ nfc_osc_client.py
@@ -2,22 +2,28 @@
 nfc ocs server
 """
 
-import nfc.clf
+import nfc
+import nfc.clf.device
+import nfc.clf.transport
 
-class NfcReader: 
-    """Nfc Neader Class"""
-    
-    def __init__(self) -> None:
-        self.port = None
-        self.
+import errno
 
 
-class NfcController:
-    """NfcController Class"""
+def discover_readers():
+    """Discover devices"""
+    clfs = []
+    for dev in nfc.clf.transport.TTY.find("tty")[0]:
+        path = "tty:{0}".format(dev[8:])
+        try:
+            clf = nfc.ContactlessFrontend(path)
+            clfs.append(clf) 
+            print("found %s" % clf.device)
+        except IOError as error:
+            if error.errno == errno.EACCES:
+                print("access denied")
+            elif error.errno == errno.EBUSY:
+                print("busy")
 
-    def __init__(self):
-        self.
 
-    def discover_readers(self):
-        """temp"""
-        pass
+if __name__ == "__main__":
+    discover_readers()
